c838f1a793b73d84a5199522d96de51e96858984
Adam Hwung <Ahwung@gmail.com>
2021-04-21 14:10:25 -0700

    Finished Section 2

--- python-exercises.py
+++ python-exercises.py
@@ -1,1 +1,11 @@
-print('Adam')
+# ==================================
+#   Section 2: Python Introduction
+# ==================================
+
+# print('Adam Hwung')
+
+name = input('What is your name?')
+print('Hello ' + name)
+
+# ----------------------------------
+
